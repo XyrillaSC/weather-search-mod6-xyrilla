0482757d411a37c2011979aa8810640b1771291a
XyrillaSC <professionalstevenkramer@gmail.com>
2023-04-12 19:06:34 -0400

    All func -icon

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -12,24 +12,34 @@ function displayHistory() {
         let historyItem = document.createElement('li')
         historyItem.textContent = city
         historyItem.classList.add('dropdown-item')
+        historyItem.setAttribute("onclick", `setInput('${city}')`)
         historyList.appendChild(historyItem)
     })
 }
+
+function setInput(city) {
+    input.value = city
+}
+
+
+
 // handles submitions and makes the api call to get data
 function handleSubmit(event) {
+    event.preventDefault();
     let city = input.value.trim()
-    event.preventDefault()
+    city = city.toLowerCase()
     if (city !== "") {
-        historyItems.push(city)
-        localStorage.setItem('history', JSON.stringify(historyItems))
-        city = city.toLowerCase()
-        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=f75e10cea0f691b69cc9dce7cec926a9`)
+        if (historyItems.includes(`${city}`) === false) {
+            historyItems.push(city)
+            localStorage.setItem('history', JSON.stringify(historyItems))
+        }
+        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=f75e10cea0f691b69cc9dce7cec926a9&units=imperial`)
             .then(function (response) {
                 return response.json()
             })
             .then(function (data) {
                 console.log(data)
-                fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=f75e10cea0f691b69cc9dce7cec926a9`)
+                fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=f75e10cea0f691b69cc9dce7cec926a9&units=imperial`)
                     .then(function (response) {
                         return response.json()
                     })
